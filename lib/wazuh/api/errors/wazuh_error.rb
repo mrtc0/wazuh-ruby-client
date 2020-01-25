a14afba9d7ab26e54e5f2b72372b938302ce9fc0
mrtc0 <kohei.morita@pepabo.com>
2020-01-25 19:23:06 +0900

    return a response error with error message

--- lib/wazuh/api/errors/wazuh_error.rb
+++ lib/wazuh/api/errors/wazuh_error.rb
@@ -2,11 +2,11 @@ module Wazuh
   module Api
     module Errors
       class WazuhError < ::Faraday::Error
-        attr_reader :response
+        attr_reader :response, :message
 
         def initialize(message, response = nil)
+          @message = message
           @response = response
-          super message
         end
       end
     end
--- lib/wazuh/faraday/response/raise_error.rb
+++ lib/wazuh/faraday/response/raise_error.rb
@@ -4,6 +4,9 @@ module Wazuh
     module Response
       class RaiseError < ::Faraday::Response::Middleware
         def on_complete(env)
+          return if env.status == 200 || env.body['error'] == 0
+          error_message = env.body['message']
+          raise Wazuh::Api::Errors::WazuhError.new(error_message, env.response)
         end
       end
     end
